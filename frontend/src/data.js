f9bb34bdfaa5c4433fd9aeabba3e611c035c2de3
KuzscoTech <kuzscotech@gmail.com>
2020-12-26 18:07:07 -0800

    Added React Router & Components Page

---
+++ frontend/src/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Bratleboro Limited Watch',
+            category: "Watch",
+            image: "/images/p1.jpg",
+            price: 120,
+            countInStock: 10,
+            brand: 'Bratleboro',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '2',
+            name: 'Apple Smart Watch',
+            category: "Watch",
+            image: "/images/p2.jpg",
+            price: 450,
+            countInStock: 20,
+            brand: 'Apple',
+            rating: 3.5,
+            numReviews: 30,
+            description: 'high quality product'
+        },
+        {
+            _id: '3',
+            name: 'Samsung Smart Watch',
+            category: "Watch",
+            image: "/images/p3.jpg",
+            price: 200,
+            countInStock: 0,
+            brand: 'Samsung',
+            rating: 4.9,
+            numReviews: 100,
+            description: 'high quality product'
+        },
+        {
+            _id: '4',
+            name: 'Huawei Smart Watch',
+            category: "Watch",
+            image: "/images/p4.jpg",
+            price: 190,
+            countInStock: 15,
+            brand: 'Huawei',
+            rating: 3.8,
+            numReviews: 70,
+            description: 'high quality product'
+        },
+        {
+            _id: '5',
+            name: 'FitBit Sport Watch',
+            category: "Watch",
+            image: "/images/p5.jpg",
+            price: 430,
+            countInStock: 50,
+            brand: 'FitBit',
+            rating: 4.6,
+            numReviews: 70,
+            description: 'high quality product'
+        },
+        {
+            _id: '6',
+            name: 'Google Smart Watch',
+            category: "Watch",
+            image: "/images/p6.jpg",
+            price: 640,
+            countInStock: 70,
+            brand: 'Google',
+            rating: 3.4,
+            numReviews: 90,
+            description: 'high quality product'
+        },
+    ]
+};
+
+export default data;
